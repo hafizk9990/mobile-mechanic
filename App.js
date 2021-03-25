994bd878e519f24c22d8240cf440e77dc5af9da6
uzair9 <studies9990@gmail.com>
2021-03-25 16:11:22 +0500

    Firebase Set-Up Done!

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import './snippets/firebaseInit'
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style = {styles.container}>
       <Text> Hello, World! </Text>
-      <StatusBar style="auto" />
+      <StatusBar style = "auto" />
     </View>
   );
 }
