b452be4b5040d2e90b0939670e8ff76c0ea2ec2c
M. Uzair Afzal <51513047+uzair9@users.noreply.github.com>
2021-03-27 02:07:21 +0500

    Update App.js

--- App.js
+++ App.js
@@ -10,12 +10,4 @@ const App = () => {
     );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 export default App
