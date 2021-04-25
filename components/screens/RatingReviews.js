17d91fbb1c49930881f06ce2c3499730fa223d07
Amna Khalid <amnakhalid607@gmail.com>
2021-04-25 12:55:50 +0500

    Commit

--- components/screens/RatingReviews.js
+++ components/screens/RatingReviews.js
@@ -77,28 +77,41 @@ const RatingReviews = (navigationProps) => {
         {(formikProps) => {
           return (
             <View style={myStyles.form}>
+              <Text style = { myStyles.title  }> Review </Text>
+              <Text style = { myStyles.subtitle  }> Rate & Review </Text>
+
+              {/* Star Rating */}
               <TextInput
-               style = { myStyles.inputField }  //change to star rating
+               style = { myStyles.starRating }  //change to star rating
                placeholder = "Enter Your Rating"
                onChangeText = { formikProps.handleChange('rate') }
                value = { formikProps.values.rate }
                keyboardType = 'numeric'
               />
+
+              {/* reviewBox */}
               <TextInput
                 multiline
-                style={myStyles.inputField}
-                placeholder="Enter Your Review"
+                style={myStyles.reviewBox}
+                placeholder="Please type your review here"
                 onChangeText={formikProps.handleChange("review")}
                 value={formikProps.values.review}
               />
               <TouchableOpacity
-                style={myStyles.loginScreenButton}
+                style={myStyles.postButton}
                 onPress={formikProps.handleSubmit}
                 underlayColor="#fff"
               >
                 <Text style={myStyles.loginText}>Post Review</Text>
               </TouchableOpacity>
               {/* add cancel button */}
+              <TouchableOpacity
+                style={myStyles.cancelButton}
+                onPress={formikProps.handleSubmit}
+                underlayColor="#fff"
+              >
+                <Text style={myStyles.loginText}>Cancel</Text>
+              </TouchableOpacity>
             </View>
           );
         }}
@@ -110,17 +123,32 @@ const myStyles = StyleSheet.create({
     form: {
       textAlign: "center",
     },
-    inputField: {
+    starRating: {
       padding: 6,
       textAlign: "center",
+      color: "red"
+    },
+    reviewBox: {
+      marginTop: 20,
+      height: 120,
+      width: '86%',
+      margin: 2,
+      borderWidth: 1,
+      padding: 5,
+      textAlignVertical: 'top',
+      color: "black",
+      borderRadius: 10,
+      marginLeft: '7%',
+      marginRight: '7%',
+      marginBottom: 10
     },
     continueButton: {
       backgroundColor: "red",
     },
-    loginScreenButton: {
-      marginRight: 40,
-      marginLeft: 40,
-      marginTop: 10,
+    postButton: {
+      marginRight: '18%',
+      marginLeft: '18%',
+      marginTop: 30,
       paddingTop: 10,
       paddingBottom: 10,
       backgroundColor: "#35b8b6",
@@ -128,12 +156,41 @@ const myStyles = StyleSheet.create({
       borderWidth: 1,
       borderColor: "#fff",
       position: "relative",
+      borderRadius: 30,
+    },
+    cancelButton: {
+      marginRight: '18%',
+      marginLeft: '18%',
+      marginTop: 10,
+      paddingTop: 10,
+      paddingBottom: 10,
+      backgroundColor: "#7f7f7f",
+      borderRadius: 10,
+      borderWidth: 1,
+      borderColor: "#fff",
+      position: "relative",
+      borderRadius: 30,
     },
     loginText: {
+      fontSize: 15,
       color: "#fff",
       textAlign: "center",
       paddingLeft: 10,
       paddingRight: 10,
+    },
+    title: {
+      fontWeight: 'bold',
+      fontSize: 35,
+      marginTop: '12%',
+      textAlign: 'center',
+      padding: 15
+    },
+    subtitle: {
+      borderTopWidth: 1,
+      fontSize: 25,
+      marginTop: '4%',
+      textAlign: 'left',
+      padding: 15
     },
     button: {
       backgroundColor: "#00aeef",
@@ -146,5 +203,6 @@ const myStyles = StyleSheet.create({
       fontSize: 12,
       textAlign: "center",
     },
+    
   });
 export default RatingReviews
